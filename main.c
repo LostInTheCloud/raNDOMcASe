c0b02a0d1129af34e8746529c08968026d3b4956
Lenni Hein <lennihein@outlook.com>
2019-07-29 15:14:41 +0000

    init setup

---
+++ main.c
@@ -0,0 +1,10 @@
+//
+// Created by lenni on 7/29/19.
+//
+
+#include "main.h"
+
+int main(int argc, char **argv)
+{
+    printf("Hello race!\n");
+}
---
+++ main.h
@@ -0,0 +1,11 @@
+//
+// Created by lenni on 7/29/19.
+//
+
+#ifndef RANDOMCASE_MAIN_H
+#define RANDOMCASE_MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#endif //RANDOMCASE_MAIN_H
